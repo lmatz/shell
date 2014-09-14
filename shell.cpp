27a5230288f6a8da97074be5faf495eebf495371
martin823 <martin823@126.com>
2014-09-14 23:56:39 +0800

    Create shell.cpp

---
+++ shell.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "Interpreter.h"
+
+
+
+int main() {
+//	signal(SIGINT,SIG_IGN);
+//	signal(SIGTERM,SIG_IGN);
+//	signal(SIGQUIT,SIG_IGN);
+//	signal(SIGTSTP,SIG_IGN);
+	printf("----------------------shell start------------------------\n");
+	while (1) {
+		prompt();
+		readthisline();
+	}
+}
