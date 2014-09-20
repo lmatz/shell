321112ae0a7cfed2e1dee3ac7d1dafdad0766e43
martin823 <martin823@126.com>
2014-09-21 00:19:51 +0800

    Create Builtin.h

---
+++ Builtin.h
@@ -0,0 +1,24 @@
+/*
+ * Builtin.h
+ *
+ *  Created on: 2014年9月20日
+ *      Author: liuzhi
+ */
+
+#ifndef BUILTIN_H_
+#define BUILTIN_H_
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
+#include <vector>
+#include <deque>
+#include "Interpreter.h"
+
+using namespace std;
+
+
+string getCurrentDic();
+
+#endif /* BUILTIN_H_ */
