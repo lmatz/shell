d6729d3e3d5d62111d558b913c5aaed3dd5a2ed7
martin823 <martin823@126.com>
2014-09-23 21:30:32 +0800

    Create execute.h

---
+++ execute.h
@@ -0,0 +1,37 @@
+#ifndef EXECUTE_H_
+#define EXECUTE_H_
+
+
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
+#include <vector>
+#include <deque>
+#include "interpreter.h"
+#include "builtin.h"
+
+extern char * commandstring[100];
+
+
+void execute() ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* EXECUTE_H_ */
