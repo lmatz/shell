a43ab07f0cdf0104b13cd86977aafab41b475208
martin823 <martin823@126.com>
2014-10-18 10:37:04 +0800

    Update shell.cpp

--- shell.cpp
+++ shell.cpp
@@ -1,3 +1,11 @@
+//
+//  main.cpp
+//  shell
+//
+//  Created by 刘智 on 14-10-12.
+//  Copyright (c) 2014年 刘智. All rights reserved.
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,23 +14,20 @@
 #include "Builtin.h"
 #include "execute.h"
 
-
+bool debug=true;
 
 
 int main() {
-//	signal(SIGINT,SIG_IGN);
-//	signal(SIGTERM,SIG_IGN);
-//	signal(SIGQUIT,SIG_IGN);
-//	signal(SIGTSTP,SIG_IGN);
-	printf("-----------------------shell start------------------------\n");
-	while (1) {
-		prompt();
-		if (readthisline()){
-			execute();
-		}
-	}
+//    	signal(SIGINT,SIG_IGN);
+    	signal(SIGTERM,SIG_IGN);
+    	signal(SIGQUIT,SIG_IGN);
+    	signal(SIGTSTP,SIG_IGN);
+    printf("-----------------------shell start------------------------\n");
+    while (1) {
+        prompt();
+        if (readthisline()){
+            execute();
+        }
+    }
+    return 0;
 }
-
-
-
-
