a84540707e23ee52d372ab1129c9306521bc5d91
martin823 <martin823@126.com>
2014-10-18 10:38:57 +0800

    Update Interpreter.h

--- Interpreter.h
+++ Interpreter.h
@@ -1,27 +1,38 @@
+//
+//  Interpreter.h
+//  shell
+//
+//  Created by 刘智 on 14-10-12.
+//  Copyright (c) 2014年 刘智. All rights reserved.
+//
+
 /*
-99: wrong input;
-15: not determined
-1 : '|'
-2 : '<'
-3 : '>'
-4 : '>>'
-5 : filename or arg or command
-6 : built-in or arg or filename
-7 : arg with star
-8 : command
-9 : built-in
-10: input filename
-11: output filename
-12: arg without star
+ 99: wrong input;
+ 15: not determined
+ 1 : '|'
+ 2 : '<'
+ 3 : '>'
+ 4 : '>>'
+ 5 : filename or arg or command
+ 6 : built-in or arg or filename
+ 7 : arg with star
+ 8 : command
+ 9 : built-in
+ 10: input filename
+ 11: output filename
+ 12: arg without star
+ 13: empty
+ filename : 5 || 6
+ arg      : 5 || 6 || 7
+ built-in : 6
+ command  : 5
+ */
 
-filename : 5 || 6
-arg      : 5 || 6 || 7
-built-in : 6
-command  : 5
 
-*/
-#ifndef INTERPRETER_H_
-#define INTERPRETER_H_
+
+#ifndef __shell__Interpreter__
+#define __shell__Interpreter__
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -33,30 +44,38 @@ command  : 5
 
 using namespace std;
 
-static vector<string> symbol;
-static char *current;
-static char commandline[256];
+extern bool debug;
+extern bool suspend;
+
+
+
 extern char *commandstring[100];
-static int token_type[100];
 extern int num_com;
-static char * token;
-static char * next;
-static int num_current=0;
-static int cd_arg=0;
-static bool error_exception=false;
+extern int token_type[100];
+extern int pipe_num;
+extern char * argument[3][100];
+extern vector<string> symbol;
+extern char *current;
+extern char commandline[256];
+extern char * token;
+extern char * next;
+extern int num_current;
+extern int cd_arg;
+extern bool error_exception;
 
-
-
+extern int pipe_pos[3];
+extern int arg_len[3];
 
 void format_print();
 void checktype();
 int check_commandname(char * str);
 void delete_command();
 void prompt();
-void tokenize();
+bool tokenize();
 bool readthisline();
 bool checkpipe();
-bool term(char *string);
+bool checkargument();
+//bool term(char *string);
 bool start();
 bool s1();
 bool s2();
@@ -66,4 +85,7 @@ bool recursive();
 bool command();
 bool term();
 
-#endif
+
+
+
+#endif /* defined(__shell__Interpreter__) */
