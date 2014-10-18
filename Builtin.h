4fa241df1cdb63c5b64139e163d1e175255e716f
martin823 <martin823@126.com>
2014-10-18 10:37:46 +0800

    Update Builtin.h

--- Builtin.h
+++ Builtin.h
@@ -1,12 +1,13 @@
-/*
- * Builtin.h
- *
- *  Created on: 2014年9月20日
- *      Author: liuzhi
- */
+//
+//  Builtin.h
+//  shell
+//
+//  Created by 刘智 on 14-10-12.
+//  Copyright (c) 2014年 刘智. All rights reserved.
+//
 
-#ifndef BUILTIN_H_
-#define BUILTIN_H_
+#ifndef __shell__Builtin__
+#define __shell__Builtin__
 
 #include <stdio.h>
 #include <iostream>
@@ -20,11 +21,16 @@
 
 using namespace std;
 
+extern bool debug;
+
 extern char * commandstring[100];
 
 
 string getCurrentDic();
 void builtin_cd();
 void builtin_exit();
+void builtin_fg();
+void builtin_jobs();
 
-#endif /* BUILTIN_H_ */
+
+#endif /* defined(__shell__Builtin__) */
